6529c0af86c858f9ea9ea39fe3eb141ee27d6836
Eduardo B. Costa <ebritto25@gmail.com>
2019-09-04 19:52:54 -0300

    RottenScrapper finalizado

--- codigos/webscrapper/rottenscrapper.py
+++ codigos/webscrapper/rottenscrapper.py
@@ -8,18 +8,69 @@ class RottenScrapper(Scrapper):
     
     def __str__(self):
         return self.dom.text
-    
-    def get_essa_semana(self):
-        div_em_cartaz = self.dom.find(id = 'Opening')
+
+    def get_dados_coluna_cinema(self, id_secao):
+        div_cinema = self.dom.find(id = id_secao)
 
         dados = []
 
-        for linha in div_em_cartaz.find_all('tr','sidebarInTheaterOpening'):
-            print(linha)
-            nota = linha.find(class_ = 'left_col').find(class_ = 'tMeterScore').text or 'Sem nota'
-            nome = linha.find(class_ = 'middle_col').a.text
-            lancamento = linha.find(class_ = 'right_col').a.text
+        for linha in div_cinema.find_all('tr'):
 
-            dados.append((nota,nome,lancamento))
+            try:
+                nota = linha.find(class_ = 'left_col').find(class_ = 'tMeterScore').text
+            except AttributeError as ex:
+                nota = 'Sem nota'
+
+            nome = linha.find(class_ = 'middle_col').a.text
+            lancamento_bilheteria = linha.find(class_ = 'right_col').a.text.strip(' \n\t')
+
+            dados_filme = tuple(map(self.remove_espaco,(nota, nome, lancamento_bilheteria)))
+
+            dados.append(dados_filme)
 
         return dados
+
+    def get_dados_coluna_tv(self,id_secao):
+
+        if id_secao == 'New-On-Tv':
+            div_tv = self.dom.find('h2',string='New TV Tonight').parent.table
+        else:
+            div_tv = self.dom.find(id = id_secao)
+
+        dados = []
+
+        for linha in div_tv.find_all('tr'):
+
+            try:
+                nota = linha.find(class_ = 'left_col').find(class_ = 'tMeterScore').text
+            except AttributeError as ex:
+                nota = 'Sem nota'
+
+            nome = linha.find(class_ = 'middle_col').a.text
+
+            dados_filme = tuple(map(self.remove_espaco,(nota, nome)))
+
+            dados.append(dados_filme)
+
+        return dados
+
+    def remove_espaco(self, texto):
+        return texto.strip(' \n\t')
+
+    def get_estreias_semana(self):
+        return self.get_dados_coluna_cinema('Opening')
+
+    def get_top_bilheteria(self):
+        return self.get_dados_coluna_cinema('Top-Box-Office')
+
+    def get_logo_cinemas(self):
+        return self.get_dados_coluna_cinema('Top-Coming-Soon')
+
+    def get_novo_tv(self):
+        return self.get_dados_coluna_tv('New-On-Tv')
+    
+    def get_tv_popular(self):
+        return self.get_dados_coluna_tv('homepage-tv-bottom')
+
+    def get_top_dvd_streaming(self):
+        return self.get_dados_coluna_tv('Top-DVD')
