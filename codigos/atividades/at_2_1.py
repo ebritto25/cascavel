23f977b39cd969b8a6888d263f3c947b873e2513
Eduardo B. Costa <ebritto25@gmail.com>
2019-09-15 19:37:32 -0300

    Att mutáveis e atividades controle

---
+++ codigos/atividades/at_2_1.py
@@ -0,0 +1,8 @@
+var = True #Declaração da variável
+
+if var:
+    #Se a variável for verdadeira o programa segue esse bloco
+    print('A variável é verdadeira!')
+else:
+    #Se a variável for falsa o programa segue esse bloco
+    print('A variável é falsa!')
---
+++ codigos/atividades/at_2_2.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número:\n')) #Lê o número e faz a conversão 
+
+if numero < 0:
+    print('O número é negativo')
+elif numero > 0:
+    print('O número é positivo')
+else:
+    print('O número é zero')
